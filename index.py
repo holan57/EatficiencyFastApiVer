727f457f1b9f45f4ecfb3a53b5f62963bfd30fa7
unknown <hoogan57@gmail.com>
2026-06-01 15:46:05 +0800

    優化網頁讀取時間與手機排版

--- index.py
+++ index.py
@@ -182,7 +182,7 @@ def root():
         <head>
             <title>Eatficiency Dashboard</title>
             <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <script src="https://cdn.tailwindcss.com"></script>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
             <style>
